9bbf556c376aca8d50bab88c6665290d5736fc19
nwhang1 <nicholasjwhang@gmail.com>
2024-03-20 09:37:04 -0700

    utilized Console.WriteLine() to create an intro

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            // String interpolation request
+            string intro = $"This is {name}, their favorite color is {color}, their favorite animal is {animal}, and their favorite band is {band}!";
+            Console.WriteLine(intro);
         }
     }
 }
